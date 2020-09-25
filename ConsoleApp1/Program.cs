e267a075aca332c2c6ac26da22c08ffb8bbb9d2b
asalmero <71348959+asalmero@users.noreply.github.com>
2020-09-25 15:01:07 -0500

    Project Created
    VS SLN Created

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            string apellidos;
+            int Edad;
+            string Ocupación;
+            string correo;
+        }
+    }
+}
