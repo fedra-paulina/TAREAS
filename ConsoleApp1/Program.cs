f21f80df607f74bb24d8404e51afb5bde0fefebe
fedra-paulina <paulinasantana_m.ibm@upci.edu.mx>
2020-09-26 16:42:13 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string Nombre;
-            string apellidos;
-            int Edad;
-            string Ocupación;
-            string correo;
+           string Nombre, Telefono, Correo, Ocupación;
+
+            Console.WriteLine("Cual es tu Nombre");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("introduce tu numero telefonico");
+            Telefono = Console.ReadLine();
+
+            Console.WriteLine("introduce tu correo");
+            Correo = Console.ReadLine();
+
+            Console.WriteLine("Ocupáción");
+            Ocupación = Console.ReadLine();
+
+            int e;
+            Console.WriteLine("cual es tu edad");
+            e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(e);
+            if (e <= 17)
+
+            {
+                Console.WriteLine("estas chavo");
+
+            }
         }
     }
+
 }
+
